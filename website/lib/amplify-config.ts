3b50f5124a8c8918121cb6e281fee2d208280098
AleAro <aarouesty@pastpost.com>
2025-11-10 17:38:53 -0600

    rotating api key

--- website/lib/amplify-config.ts
+++ website/lib/amplify-config.ts
@@ -1,6 +1,14 @@
 import { Amplify } from 'aws-amplify';
-import config from '../amplifyconfiguration.json';
 
-Amplify.configure(config, { ssr: true });
+Amplify.configure(
+  {
+    aws_project_region: process.env.NEXT_PUBLIC_AWS_REGION || 'us-east-1',
+    aws_appsync_region: process.env.NEXT_PUBLIC_AWS_REGION || 'us-east-1',
+    aws_appsync_authenticationType: 'API_KEY',
+    aws_appsync_apiKey: process.env.NEXT_PUBLIC_APPSYNC_API_KEY,
+    aws_appsync_graphqlEndpoint: process.env.NEXT_PUBLIC_APPSYNC_ENDPOINT || 'https://5xvy4x6rgjh5fmklzs6vgrwiqq.appsync-api.us-east-1.amazonaws.com/graphql',
+  } as any, // Suppress type error for extra property
+  { ssr: true }
+);
 
 export default Amplify;
